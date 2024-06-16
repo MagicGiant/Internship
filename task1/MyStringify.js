b0a5d7a70e287e52713e4700b1070b87d0a0a55b
Sherka <obidjanow@mail.ru>
2024-06-17 00:16:46 +0300

    refactoring

--- task1/MyStringify.js
+++ task1/MyStringify.js
@@ -1,120 +1,134 @@
-// Как по мне это лучшее решение, так как оно полностью расширяемое, красивое(не использует уродские свитч кейсы), очень быстро работает (без перебора типов) за счет хеш мапы, а так же не выкинет ошибку в случае, если какой-то тип объекта не был рассмотрен (скорее всего просто покажет [object Object])
-
-// Проблема, которую я заметил. Нельзя задать одинаковое поведение в одном сете для нескольких типов Объектов. Для каждого типа нужен отдельный сет. Возможно так даже лучше, но код при расширении может получиться громоздким. 
-// Можно решить проблему, если изменить getObjectType определенным образом и немного по другому хранить ключи
+// Как по мне это лучшее решение, так как оно полностью расширяемое, красивое(не использует уродские свитч кейсы и глобальные переменные), очень быстро работает (без перебора типов) за счет хеш мапы, а так же не выкинет ошибку в случае, если какой-то тип объекта не был рассмотрен (скорее всего просто покажет [object Object])
 
 
+function MyStringify(value, space = 0){
+
+    if (space === 0){
+        MyStringify.newLine = '';
+        MyStringify.keyValueSpace = '';
+    }
+    else{
+        MyStringify.newLine = '\n';
+        MyStringify.keyValueSpace = ' '
+    }
 
 
-let strigifyRules = new Map();
+    return MyStringify.recursiveCast(value, space);
+}
 
-let spaceCount = 0;
-let newLine = ''
-let keyValueSpace = ''
 
- 
+MyStringify.spaceCount = 0;
 
-function getObjectType(obj){
+MyStringify.strigifyRules = new Map();
+
+
+MyStringify.getObjectType = (obj) => {
     return Object.prototype.toString.call(obj).match(/\w*(?=])/)[0];
 }
 
-function getSpaces(){
-    return " ".repeat(spaceCount);
+MyStringify.getSpaces = () => {
+    return " ".repeat(MyStringify.spaceCount);
 }
 
-
-function myStringify(value, space = 0){
-    if (space != 0){
-        newLine = '\n';
-        keyValueSpace = " ";
-    }
-
-    let type = getObjectType(value);
-
-
-    if (!strigifyRules.has(type)){
-        return getSpaces() + value.toString();
-    }
-
-    return strigifyRules.get(type)(value, space);
-}
-
-function deleteLastComma(str){
+MyStringify.deleteLastComma = (str) =>{
     if (str.lastIndexOf(",") !== -1) {
         return str.slice(0, str.lastIndexOf(",")) + str.slice(str.lastIndexOf(",") + 1);
     }
     return str;
+}
+
+MyStringify.recursiveCast = (value, space) =>{
+
+    let type = MyStringify.getObjectType(value);
+
+
+    if (!MyStringify.strigifyRules.has(type)){
+        return MyStringify.getSpaces() + value.toString();
+    }
+
+    return MyStringify.strigifyRules.get(type)(value, space);
 }
   
 
 // ________set_rules_________
 
 
-strigifyRules.set('Function', obj =>{
+MyStringify.strigifyRules.set('Function', obj =>{
     return null;
 })
 
 
-strigifyRules.set('Set', obj => {
+MyStringify.strigifyRules.set('Set', obj => {
     return '{}';
 })
 
-strigifyRules.set('Map', obj => {
+MyStringify.strigifyRules.set('Map', obj => {
     return '{}';
 })
 
-strigifyRules.set('String', obj => {
+MyStringify.strigifyRules.set('String', obj => {
     return `"${obj}"`
 })
 
-strigifyRules.set('Date', obj => {
+MyStringify.strigifyRules.set('Date', obj => {
     let date = new Date();
     return `"${obj.toISOString()}"`
 })
 
-strigifyRules.set('Array', (obj, space) => {
-    let result = getSpaces() + '[' + newLine;
+MyStringify.strigifyRules.set('Array', (obj, space) => {
+    let result = MyStringify.getSpaces() + '[' + MyStringify.newLine;
 
-    spaceCount += space;
+    MyStringify.spaceCount += space;
     for (value of obj){
-        result += myStringify(value, space) + ',' + newLine;
+        result += MyStringify.recursiveCast(value, space) + ',' + MyStringify.newLine;
     }
 
-    result = deleteLastComma(result);
+    result = MyStringify.deleteLastComma(result);
 
-    spaceCount -= space;
+    MyStringify.spaceCount -= space;
 
-    return result + getSpaces() + ']';
+    return result + MyStringify.getSpaces() + ']';
 })
 
-strigifyRules.set('Object', (obj, space) => {
-    let result = getSpaces() + '{' + newLine;
+MyStringify.strigifyRules.set('Object', (obj, space) => {
+    let result = MyStringify.getSpaces() + '{' + MyStringify.newLine;
 
-    spaceCount += space;
+    MyStringify.spaceCount += space;
 
     for (key in obj){
-        result += `${getSpaces()}"${key}":${myStringify(obj[key], space)},${newLine}`;
+        result += `${MyStringify.getSpaces()}"${key}":${MyStringify.recursiveCast(obj[key], space)},${MyStringify.newLine}`;
     };
 
-    spaceCount -= space;
+    MyStringify.spaceCount -= space;
 
-    result = deleteLastComma(result);
+    result = MyStringify.deleteLastComma(result);
 
-    return result + getSpaces() + '}';
+    return result + MyStringify.getSpaces() + '}';
 })
 
 
 // _________testing____________
 
-function testMyStringify(testArray=[[1,2,3,[1,2,3]], a], space = 4){
+function testMyStringify(
+    testArray=[
+        
+        [1,2,3,[1,2,3]],
 
+        {
+            1: "1",
+            2: [1, 2, {1: "3"}]
+        },
+
+        new Array(3),],
+    space = 4)
+{
     function logLine(){
         console.log("_________________________");
     }
 
     for (value of testArray){
         const standart = JSON.stringify(value, null, space);
-        const my = myStringify(value, space);
+        const my = MyStringify(value, space);
         if (standart === my){
             console.log("OK!");
         }
@@ -129,47 +143,3 @@ function testMyStringify(testArray=[[1,2,3,[1,2,3]], a], space = 4){
         logLine();
     }
 }
-
-
-let a = {
-    hello : "a",
-    b : [1, 2, 3, 4],
-    c: {
-        a: "a",
-    }
-}
-
-let arr = [
-    1,
-    2,
-    function(){console.log("hello");}
-]
-
-let map = new Map();
-
-let set = new Set();
-
-set.add(1);
-
-set.add(2);
-
-let number = new Number(3);
-
-function f(params) {}
-
-class User {
-
-    constructor(name) {
-        this.name = name;
-    }
-
-    sayHi() {
-        alert(this.name);
-    }
-
-}
-
-let user = new User("Иван");
-let date = new Date();
-
-testMyStringify()
