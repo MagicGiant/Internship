fae55e288598537df1f2fbeef419c5a6feafb79e
Sherka <obidjanow@mail.ru>
2024-06-22 23:04:49 +0300

    added the ability to open files

--- task3/server.js
+++ task3/server.js
@@ -13,35 +13,37 @@ app.use(express.static('css'));
 
 const createViewPath = (page) => path.resolve(__dirname, 'views', `${page}.ejs`);
 
+renderPath = (res, filePath) =>{
+    
+    fs.stat(filePath, (err, stats) =>{
+        if (stats.isFile()){
+            res.sendFile(filePath);
+        }
+        else{
+            fs.readdir(filePath, (err, files) => {
+                if (err){
+                    console.error('Error reading static directory', err);
+                    res.status(500).send('Server error');
+                }
+                else
+                    res.render(createViewPath('index'), {files});
+            });
+        }
+    })
+} 
 
 app.listen(PORT, err => {
     err ? console.log(err) : console.log(`http://localhost:${PORT}/`)
 });
 
 app.get('/', (req, res) => {
-    fs.readdir(staticPath, (err, files) => {
-        if (err){
-            console.error('Error reading static directory', err);
-            res.status(500).send('Server error');
-        }
-        else{
-            res.render(createViewPath('index'), {files});
-        }   
-    })
+    renderPath(res, staticPath);
 });
 
 app.get('/*', (req, res) => {
     const filePath = path.join(staticPath, req.params[0]);
 
-    fs.readdir(filePath, (err, files) => {
-        if (err){
-            console.error('Error reading static directory', err);
-            res.status(500).send('Server error');
-        }
-        else{
-            res.render(createViewPath('index'), {files});
-        }   
-    })
+    renderPath(res, filePath);
 });
 
 app.use((req, res) => {
